adeae48cbb5d6eecb48b9696c5585ce54c9a3233
Faris Huskovic <faris@coder.com>
2021-03-26 21:27:51 -0500

    speech-output doesn't trigger if err != nil so no point in calling Say() in those blocks

--- main.go
+++ main.go
@@ -12,16 +12,13 @@ import (
 )
 
 func main() {
-	lambda.Start(func(ctx context.Context) (AlexaResponse, error) {
+	lambda.Start(func(ctx context.Context) (*AlexaResponse, error) {
 		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 		defer cancel()
 
-		resp := newResponse()
-
 		envVars, err := loadEnvVars(ctx)
 		if err != nil {
-			resp.Say("hmmm, it looks like I couldn't load your environment variables")
-			return resp, xerrors.Errorf("failed to load environment variables: %w", err)
+			return nil, xerrors.Errorf("failed to load environment variables: %w", err)
 		}
 
 		var (
@@ -32,8 +29,7 @@ func main() {
 
 		url, err := url.Parse(envVars["CODER_ACCESS_URL"])
 		if err != nil {
-			resp.Say("hmmm, it looks I couldn't parse the coder access url you set")
-			return resp, xerrors.Errorf("failed to parse CODER_ACCESS_URL: %w", err)
+			return nil, xerrors.Errorf("failed to parse CODER_ACCESS_URL: %w", err)
 		}
 
 		client, err := coder.NewClient(
@@ -45,21 +41,19 @@ func main() {
 		)
 
 		if err != nil {
-			resp.Say("hmmm, it looks I couldn't initialize the coder client")
-			return resp, xerrors.Errorf("failed to initialize coder client: %w", err)
+			return nil, xerrors.Errorf("failed to initialize coder client: %w", err)
 		}
 
 		env, err := getUserEnv(ctx, client, email, envName)
 		if err != nil {
-			resp.Say(fmt.Sprintf("hmmm, it looks I couldn't find the %s environment", envName))
-			return resp, xerrors.Errorf("failed to get user environment: %w", err)
+			return nil, xerrors.Errorf("failed to get user environment: %w", err)
 		}
 
 		if err := client.RebuildEnvironment(ctx, env.ID); err != nil {
-			resp.Say("hmmm, something went wrong when I tried adding an environment build job to the queue")
-			return resp, xerrors.Errorf("failed to enqueue environment build job: %w", err)
+			return nil, xerrors.Errorf("failed to enqueue environment build job: %w", err)
 		}
+		resp := newResponse()
 		resp.Say(fmt.Sprintf("OK, I added a new environment build job to the queue for the %s environment", envName))
-		return resp, nil
+		return &resp, nil
 	})
 }
